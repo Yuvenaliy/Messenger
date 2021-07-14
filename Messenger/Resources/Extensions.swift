b764a5ebed3103f829bfa5ee5f248ad5b64cc16a
YuvaKhlopkov <yuvakhlopkov93@gmail.com>
2021-07-14 12:01:03 +0300

    Log in and Register Screen

---
+++ Messenger/Resources/Extensions.swift
@@ -0,0 +1,34 @@
+//
+//  Extensions.swift
+//  Messenger
+//
+//  Created by Ювеналий on 13.07.2021.
+//
+
+import UIKit
+
+extension UIView {
+    public var width: CGFloat {
+        return self.frame.size.width
+    }
+    
+    public var height: CGFloat {
+        return self.frame.size.height
+    }
+    
+    public var top: CGFloat {
+        return self.frame.origin.y
+    }
+    
+    public var bottom: CGFloat {
+        return self.frame.size.height + self.frame.origin.y
+    }
+    
+    public var left: CGFloat {
+        return self.frame.origin.x
+    }
+    
+    public var right: CGFloat {
+        return self.frame.width + self.frame.origin.x
+    }
+}
